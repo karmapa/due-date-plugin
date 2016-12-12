24fd78719c02eab069b4d6fd7a39f8481ccf8111
rickie120243 <rickie120243@gmail.com>
2016-12-12 16:06:45 +0800

    add test.js

---
+++ test/test.js
@@ -0,0 +1,10 @@
+import SQL from 'sql.js';
+import {compareDate} from './main.js';
+import assert from 'assert';
+console.log(compareDate);
+
+describe('convert', function () {
+  it('convert check', function () {
+    assert.equal('1', '1');
+  });
+});
