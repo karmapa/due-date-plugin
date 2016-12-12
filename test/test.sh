8ade071f3add2469e2200041f9a26e5c2cf021c6
rickie120243 <rickie120243@gmail.com>
2016-12-12 15:51:36 +0800

    fix replace regx

--- test/test.sh
+++ test/test.sh
@@ -3,4 +3,5 @@
 cp ./main.js ./test && cp ./sendEmail.js ./test && cp ./index.js ./test/test.js
 cd ./test
 sed -i '' 's/main/index/' test.js
-sed -i '' 's/function/export function/g' main.js
+sed -i '' 's/^function/export function/g' main.js
+sed -i '' 's/^sendEmail/\/\/sendEmail/g' main.js
