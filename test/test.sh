08d494d6d5f2c8d53a7a4d6bc690c15688476a2f
rickie120243 <rickie120243@gmail.com>
2016-12-12 15:54:42 +0800

    remove sendEmail.js

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ./main.js ./test && cp ./sendEmail.js ./test && cp ./index.js ./test/test.js
+cp ./main.js ./test && cp ./index.js ./test/test.js
 cd ./test
 sed -i '' 's/main/index/' test.js
 sed -i '' 's/^function/export function/g' main.js
