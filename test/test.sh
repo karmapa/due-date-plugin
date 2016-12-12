edccfec98b341438791443a4bfea75c469e48d5b
rickie120243 <rickie120243@gmail.com>
2016-12-12 16:07:44 +0800

    add babel for mocha

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cp ./main.js ./test && cp ./index.js ./test/test.js
+cp ./main.js ./test
 cd ./test
-sed -i '' 's/main/index/' test.js
 sed -i '' 's/^function/export function/g' main.js
 sed -i '' 's/^sendEmail/\/\/sendEmail/g' main.js
+sed -i '' 's/^import sendEmail/\/\/import sendEmail/g' main.js
+cd ..
+mocha --compilers js:babel-core/register
