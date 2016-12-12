203e9ce0098be53471d7a6bac15dc3b83f2db950
rickie120243 <rickie120243@gmail.com>
2016-12-12 16:17:52 +0800

    add replace Date

--- test/test.sh
+++ test/test.sh
@@ -5,5 +5,6 @@ cd ./test
 sed -i '' 's/^function/export function/g' main.js
 sed -i '' 's/^sendEmail/\/\/sendEmail/g' main.js
 sed -i '' 's/^import sendEmail/\/\/import sendEmail/g' main.js
+sed -i '' 's/new Date(today).getTime()/new Date("2016-12-12").getTime()/g' main.js
 cd ..
 mocha --compilers js:babel-core/register
