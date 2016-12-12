a194c1b693c3c4f8f411b6369f71d0c9dd491933
rickie120243 <rickie120243@gmail.com>
2016-12-12 18:52:47 +0800

    fix test.sh

--- test/main.js
+++ test/main.js
@@ -2,21 +2,21 @@ import fs from 'fs';
 import SQL from 'sql.js';
 import naturalSort from 'natural-sort';
 //import sendEmail from './sendEmail.js';
-const filebuffer = fs.readFileSync('../trac.db');
+//const filebuffer = fs.readFileSync('../trac.db');
 
 // Load the db
-const db = new SQL.Database(filebuffer);
-const date = new Date();
+//const db = new SQL.Database(filebuffer);
+//const date = new Date();
 
-const today = date.toISOString().split('T')[0];
-const sqlText = 'SELECT ticket_custom.ticket, ticket_custom.value ' +
-  'FROM ticket_custom, ticket ' +
-  'WHERE ticket_custom.ticket = ticket.id AND ticket_custom.value != "" AND ticket.status != "closed" ' +
-  'AND ticket_custom.name = "due_date"';
-const tickietCustom = db.exec(sqlText)[0].values;
-const tickets = compareDate(tickietCustom, db);
-const htmlText = genHtmlText(tickets);
-const pureText = genPureText(htmlText);
+//const today = date.toISOString().split('T')[0];
+//const sqlText = 'SELECT ticket_custom.ticket, ticket_custom.value ' +
+//  'FROM ticket_custom, ticket ' +
+//  'WHERE ticket_custom.ticket = ticket.id AND ticket_custom.value != "" AND ticket.status != "closed" ' +
+//  'AND ticket_custom.name = "due_date"';
+//const tickietCustom = db.exec(sqlText)[0].values;
+//const tickets = compareDate(tickietCustom, db);
+//const htmlText = genHtmlText(tickets);
+//const pureText = genPureText(htmlText);
 
 //sendEmail({from: '', to: '', subject: 'trac due date', htmlBody: htmlText, textBody: pureText});
 
